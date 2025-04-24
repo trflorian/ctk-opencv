d479a662e9471c159b6333a5e98434069895e734
trflorian <florian.trautweiler@hispeed.ch>
2025-04-24 21:39:56 +0200

    implement basic gui for ctk image display

---
+++ ctk_image_display.py
@@ -0,0 +1,125 @@
+import logging
+from PIL import Image
+import customtkinter
+import tkinter as tk
+import cv2
+import numpy as np
+import queue
+
+class CTkImageDisplay(customtkinter.CTkLabel):
+    """
+    A reusable CustomTkinter Label widget to display opencv images.
+    """
+
+    def __init__(
+        self,
+        master: any,
+        display_size: tuple[int, int] | None = None,
+        refresh_dt_ms: int = 1000 // 60,
+        *args,
+        **kwargs,
+    ):
+        self.logger = logging.getLogger(__name__)
+
+        self._textvariable = customtkinter.StringVar(master, "Loading...")
+        super().__init__(
+            master,
+            width=display_size[0] if display_size else 1024,
+            height=display_size[1] if display_size else 1024,
+            textvariable=self._textvariable,
+            image=None,
+            *args,
+            **kwargs,
+        )
+
+        self.display_size = display_size
+        self.widget_size = display_size
+        self.refresh_dt_ms = refresh_dt_ms
+
+        self._frame = None
+        self.frame_queue = queue.Queue[np.ndarray](maxsize=1)
+        self.bind("<Configure>", self._on_resize)
+
+        self.after(self.refresh_dt_ms, self._on_refresh)
+
+    def calculate_target_size(self, frame: np.ndarray) -> tuple[int, int]:
+        """
+        Calculate the target size for the image based on the display size and aspect ratio.
+        """
+        frame_width, frame_height = frame.shape[1], frame.shape[0]
+        target_width, target_height = self.display_size if self.display_size else self.widget_size
+
+        target_width = min(self.widget_size[0], target_width)
+        target_height = min(self.widget_size[1], target_height)
+
+        # make sure to keep the aspect ratio
+        if frame_width / frame_height > target_width / target_height:
+            target_height = int(frame_height * target_width / frame_width)
+        else:
+            target_width = int(frame_width * target_height / frame_height)
+
+        return target_width, target_height
+
+    def _set_frame(self, frame: np.ndarray) -> None:
+        self._frame = frame
+
+        # Calculate the display size
+        target_width, target_height = self.calculate_target_size(frame)
+
+        # Resize the frame to fit the display size
+        resized_frame = cv2.resize(
+            frame,
+            (target_width, target_height),
+            interpolation=cv2.INTER_AREA,
+        )
+
+        if resized_frame.shape[2] == 4:
+            frame_rgba = cv2.cvtColor(resized_frame, cv2.COLOR_BGRA2RGBA)
+            frame_pil = Image.fromarray(frame_rgba, mode="RGBA")
+        else:
+            frame_rgb = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
+            frame_pil = Image.fromarray(frame_rgb, mode="RGB")
+
+        ctk_image = customtkinter.CTkImage(
+            light_image=frame_pil,
+            dark_image=frame_pil,
+            size=(target_width, target_height),
+        )
+        self.configure(image=ctk_image, text="")
+        self._textvariable.set("")
+
+    def _on_refresh(self) -> None:
+        """
+        Handles the refresh event of the widget.
+        This method is called periodically to update the displayed image.
+        """
+        try:
+            frame = self.frame_queue.get_nowait()
+            self._set_frame(frame)
+        except queue.Empty:
+            pass
+
+        self.after(self.refresh_dt_ms, self._on_refresh)
+
+    def _on_resize(self, event: tk.Event) -> None:
+        """
+        Handles the resize event of the widget.
+        This method is called when the widget is resized.
+        """
+        self.widget_size = (
+            event.width,
+            event.height,
+        )
+
+        # update the displayed image if available
+        if self._image is not None and self._frame is not None:
+            self._set_frame(self._frame)
+
+    def update_frame(self, frame: np.ndarray) -> None:
+        """
+        Updates the displayed image with a new frame using CTkImage.
+
+        Args:
+            frame: The new frame to display, in BGR format.
+        """
+        self.frame_queue.put(frame)
--- main.py
+++ main.py
@@ -1,5 +1,72 @@
-def main():
-    print("Hello from ctk-opencv!")
+import logging
+import threading
+import cv2
+import customtkinter
+
+from ctk_image_display import CTkImageDisplay
+
+
+class App(customtkinter.CTk):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.title("Webcam Stream")
+        self.geometry("800x600")
+
+        radio_var = customtkinter.StringVar(value="1")
+
+        # LAYOUT: left side radio button group, right side image display
+        self.radio_frame = customtkinter.CTkFrame(self)
+        self.radio_frame.pack(side="left", fill="both", expand=True, padx=10, pady=10)
+        self.radio_button_1 = customtkinter.CTkRadioButton(
+            self.radio_frame, text="Option 1", variable=radio_var, value=1
+        )
+        self.radio_button_1.pack(padx=10, pady=10)
+        self.radio_button_2 = customtkinter.CTkRadioButton(
+            self.radio_frame, text="Option 2", variable=radio_var, value=2
+        )
+        self.radio_button_2.pack(padx=10, pady=10)
+        self.radio_button_3 = customtkinter.CTkRadioButton(
+            self.radio_frame, text="Option 3", variable=radio_var, value=3
+        )
+        self.radio_button_3.pack(padx=10, pady=10)
+
+        # LAYOUT: right side image display
+        self.image_frame = customtkinter.CTkFrame(self)
+        self.image_frame.pack(side="right", fill="both", expand=True, padx=10, pady=10)
+
+        # Create a CTkImageDisplay widget
+        self.image_display = CTkImageDisplay(self.image_frame, corner_radius=10)
+        self.image_display.pack(fill="both", expand=True, padx=10, pady=10)
+
+        self.webcam_stream_thread = threading.Thread(
+            target=self.start_webcam_stream, daemon=True
+        )
+        self.webcam_stream_thread.start()
+
+    def start_webcam_stream(self) -> None:
+        """
+        Start the webcam stream and update the image display.
+        """
+
+        cap = cv2.VideoCapture(0)
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            # Update the image display
+            self.image_display.update_frame(frame)
+
+        cap.release()
+
+
+def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+
+    app = App()
+    app.mainloop()
 
 
 if __name__ == "__main__":

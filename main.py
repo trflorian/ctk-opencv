1dcd24da97e4f6a2b21dd1b265738fa3457b8a90
trflorian <florian.trautweiler@hispeed.ch>
2025-04-24 21:55:43 +0200

    select normal by defalt

--- main.py
+++ main.py
@@ -19,11 +19,14 @@ class App(customtkinter.CTk):
         self.radio_frame = customtkinter.CTkFrame(self)
         self.radio_frame.pack(side="left", fill="both", expand=True, padx=10, pady=10)
 
-        for option in ["Normal", "Grayscale", "Blur", "Canny", "Sobel", "Laplacian"]:
+        for option in ["Normal", "Grayscale", "Blur", "Threshold", "Canny", "Sobel", "Laplacian"]:
             radio_button = customtkinter.CTkRadioButton(
                 self.radio_frame, text=option, variable=self.radio_var, value=option
             )
             radio_button.pack(padx=10, pady=10)
+
+            if option == "Normal":
+                radio_button.select()
 
         # LAYOUT: right side image display
         self.image_frame = customtkinter.CTkFrame(self)
@@ -56,6 +59,9 @@ class App(customtkinter.CTk):
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             elif filter_option == "Blur":
                 frame = cv2.GaussianBlur(frame, (15, 15), 0)
+            elif filter_option == "Threshold":
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                _, frame = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
             elif filter_option == "Canny":
                 frame = cv2.Canny(frame, 100, 200)
             elif filter_option == "Sobel":
